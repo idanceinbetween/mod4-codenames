082e80219a7da5974e56c2d38b75206a22ddf43b
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-27 13:31:50 +0100

    white background, align text centre of tile done.

--- src/components/WordCard.js
+++ src/components/WordCard.js
@@ -1,33 +1,33 @@
-import React, { Component, Fragment } from "react";
-import { Card } from "semantic-ui-react";
+import React, { Component, Fragment } from 'react'
+import { Card } from 'semantic-ui-react'
 
 class WordCard extends Component {
-  state = { hit: false };
+  state = { hit: false }
 
   checkSelect = () => {
     if (!this.props.spymasterView) {
-      this.setState({ hit: true });
-      this.props.handleCardSelect(this.props.word);
+      this.setState({ hit: true })
+      this.props.handleCardSelect(this.props.word)
     }
-  };
+  }
 
   render() {
-    const { word } = this.props;
+    const { word } = this.props
     // const { word, color } = this.props.word;
     return (
       <Fragment>
         {!this.state.hit ? (
-          <Card className="cnWord" onClick={this.checkSelect}>
+          <Card className='cnWordb tile wcn' onClick={this.checkSelect}>
             <Card.Header>{word.word}</Card.Header>
           </Card>
         ) : (
-          <Card className="cnWord">
-            <Card.Header>{""}</Card.Header>
+          <Card className='cnWord rcn'>
+            <Card.Header>{''}</Card.Header>
           </Card>
         )}
       </Fragment>
-    );
+    )
   }
 }
 
-export default WordCard;
+export default WordCard
