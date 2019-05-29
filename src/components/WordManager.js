053511126b8becf67fc47b533e64433a5df0962f
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-29 16:59:20 +0100

    added edit word function

--- src/components/WordManager.js
+++ src/components/WordManager.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import React, { Fragment } from 'react'
+import { Segment, Grid, Form } from 'semantic-ui-react'
 import AbsoluteWrapper from '../components/AbsoluteWrapper'
 
 const randomWordUrl = 'http://localhost:3007/randomword'
@@ -7,7 +7,9 @@ const wordsUrl = 'http://localhost:3007/words'
 
 class WordManager extends React.Component {
   state = {
-    word: ''
+    word: '',
+    editedWordStr: '',
+    showEditForm: false
   }
 
   componentDidMount() {
@@ -43,6 +45,38 @@ class WordManager extends React.Component {
     event.target[0].value = ''
   }
 
+  saveEdit = () => {
+    this.editWordInServer(this.state.editedWordStr)
+  }
+
+  handleEdit = () => {
+    this.setState({
+      showEditForm: !this.state.showEditForm,
+      editedWordStr: this.state.word.word
+    })
+  }
+
+  editWordInServer = string => {
+    const word = { word: string }
+    fetch(wordsUrl + `/${this.state.word.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(word)
+    })
+      .then(resp => resp.json())
+      .then(
+        this.setState(
+          {
+            showEditForm: !this.state.showEditForm,
+            word: { ...this.state.word, word: `${this.state.editedWordStr}` }
+          },
+          () => this.setState({ editedWordStr: '' })
+        )
+      )
+  }
+
   addWordToServer = string => {
     const word = { word: string }
     fetch(wordsUrl, {
@@ -54,45 +88,96 @@ class WordManager extends React.Component {
     })
   }
 
+  handleTypingChange = event => {
+    this.setState({ editedWordStr: event.target.value })
+  }
+
   render() {
     return (
       <AbsoluteWrapper>
-        <Segment raised textAlign='left'>
-          <h5>Manage the backend word list here. Experts only.</h5>
-          <p>
-            IMPORTANT: you MUST update the seed file manually after using this
-            feature. Do not erase the database before updating the seed file.
-            The seed file should reflect the current database of words.
-          </p>
-        </Segment>
-        <Segment raised textAlign='left'>
-          <h5>
-            Refine word database here. Click keep or delete to get another
-            random word to decide on.
-          </h5>
-          <h1>{this.state.word.word}</h1>
-          <button className='ui button' onClick={this.handleKeep}>
-            Keep
-          </button>
-          <button className='ui button' onClick={this.deleteWordFromServer}>
-            DESTROY
-          </button>
-        </Segment>
-        <Segment raised textAlign='left'>
-          <h1>Add a word</h1>
-          <form className='ui form' onSubmit={this.handleAdd}>
-            <div className='field'>
-              <input
-                type='text'
-                name='words'
-                placeholder='words separated by spaces and/or commas'
-              />
-            </div>
-            <button className='ui button' type='submit'>
-              Submit
-            </button>
-          </form>
-        </Segment>
+        <Grid columns={3} textAlign='center' verticalAlign='top'>
+          <Grid.Row verticalAlign='top'>
+            <Grid.Column width={3} />
+            <Grid.Column width={10}>
+              <Segment inverted color='blue' raised textAlign='left'>
+                <h5>Manage the backend word list here. Experts only.</h5>
+                <p>
+                  IMPORTANT: you MUST update the seed file manually after using
+                  this feature. Do not erase the database before updating the
+                  seed file. The seed file should reflect the current database
+                  of words.
+                </p>
+              </Segment>
+              <Segment raised textAlign='left'>
+                <h4>Refine Existing Database</h4>
+                <p>
+                  Click keep or delete to get another random word to decide on.
+                </p>
+
+                {!this.state.showEditForm && <h1>{this.state.word.word}</h1>}
+
+                {this.state.showEditForm && (
+                  <Form>
+                    <Form.Field>
+                      <input
+                        value={this.state.editedWordStr}
+                        onChange={event => this.handleTypingChange(event)}
+                      />
+                    </Form.Field>
+                  </Form>
+                )}
+
+                {!this.state.showEditForm && (
+                  <Fragment>
+                    <button className='ui button' onClick={this.handleKeep}>
+                      Keep
+                    </button>
+                    <button className='ui button' onClick={this.handleEdit}>
+                      Edit
+                    </button>
+                    <button
+                      className='ui button'
+                      onClick={this.deleteWordFromServer}
+                    >
+                      Delete
+                    </button>
+                  </Fragment>
+                )}
+
+                {this.state.showEditForm && (
+                  <Fragment>
+                    <button className='ui button' onClick={this.saveEdit}>
+                      Save Changes
+                    </button>
+                    <button className='ui button' onClick={this.handleEdit}>
+                      Cancel Edit
+                    </button>
+                  </Fragment>
+                )}
+              </Segment>
+              <Segment raised textAlign='left'>
+                <h3>Add Word/Words</h3>
+                <p>
+                  If entering more than 1 word, separate by spaces and/or
+                  commas.
+                </p>
+                <form className='ui form' onSubmit={this.handleAdd}>
+                  <div className='field'>
+                    <input
+                      type='text'
+                      name='words'
+                      placeholder='words separated by spaces and/or commas'
+                    />
+                  </div>
+                  <button className='ui button' type='submit'>
+                    Submit
+                  </button>
+                </form>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={3} />
+          </Grid.Row>
+        </Grid>
       </AbsoluteWrapper>
     )
   }
